109c59e4ed2abb341b1af0d6a886909835c63851
etienne naude <etinaude@gmail.com>
2020-09-08 11:36:26 +1200

    once crashes reruns based off last item in the list

--- find.py
+++ find.py
@@ -4,19 +4,20 @@ from page import Page
 import json
 
 # a very good place to start
-item = "ABC"
 
 articles =[]
 complete = []
 short ={}
+item = "ABC"
 
 try:
     complete = json.load( open("json/done.json", "r" ) )
+    item = complete[-1]
 except:
     pass
 
 for i in range(6152223):
-    #try:
+    try:
         current = Page(item)
         articles.extend(current.links)
         short[current.title]= current.links
@@ -24,17 +25,16 @@ for i in range(6152223):
             while articles[0] in complete:
                 articles.pop(0)
         except Exception as error:
-            print("error",error)
-            articles = ["Special:Random"]
+            print("\terror1:\t",error)
+            articles = [complete[-1]]
         item = articles[0]
         complete.append(item)
         articles.pop(0)
         print(item)
-        #print(complete)
         if (i%100==0):
             with open("json/done.json", 'w') as f:
                 json.dump(complete, f)
-    #except Exception as error:
-    #    print("error",error)
-    #    with open("json/error.json", 'w') as f:
-    #            json.dump(complete, f)
+    except Exception as error:
+        print("\terror2:\t",error)
+        with open("json/done.json", 'w') as f:
+               json.dump(complete, f)
