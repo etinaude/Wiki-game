439ec67fd7313a3f453c1a0b7dd1bdc73c179393
etienne naude <etinaude@gmail.com>
2020-09-08 01:01:57 +1200

    writing to file sorted

--- find.py
+++ find.py
@@ -4,4 +4,13 @@ import pickle
 import re
 from page import Page
 
-test= Page("https://en.wikipedia.org/wiki/Pigeonhole_sort")
+item = "Harry_Potter"
+
+articles =[]
+
+for i in range(5):
+    current = Page(item)
+    articles.extend(current.links)
+    item = articles[0]
+    articles.pop(0)
+    print(item)
--- page.py
+++ page.py
@@ -6,49 +6,28 @@ class Page:
         links=[]
         wiki_link='\href="/wiki/'
         raw = str(self.raw)
-        raw = raw.split("mw-parser-output")[1]
+        raw = raw.split('id="mw-content-text"')[1]
         #print(raw)
         raw = raw.split('id="References"')[0]
         links = re.findall('href="/wiki/\w+"', raw)
-        print(links)
-        '''try:
-            for i in range(1000):
-                index= raw.find(wiki_link)+12
-                links.append("https://en.wikipedia.org/wiki/"+ raw[index:raw.find("title",index)-2])
-                raw = raw[index+1:]
-        except:
-            pass
-        links = links[:links.index("https://en.wikipedia.org/wiki/Main_Page")]
-        i=0
-        dels = ["File","Special:","International_Standard_Book_Number","Digital_object_identifier","PubMed_Identifier","JSTOR","Help:"]
-        while i < len(links):
-            for j in dels:
-                if j in links[i]:
-                    links.remove(links[i])
-                    i-=1
-                    break
-            if "class=" in links[i]:
-                links[i] = links[i][:links[i].find("class")]
-            if "#" in links[i]:
-                links[i] = links[i][:links[i].find("class")]
-            if "\"" in links[i]:
-                links[i] = links[i][:links[i].find("class")-1]
-            i+=1
-        '''
+        for i in range(len(links)):
+            links[i]=links[i].replace('href="/wiki/',"")
+            links[i]=links[i].replace('"',"")
         self.links=links
         words=""
         for i in links[:-1]:
             if i =="":
                 break
             words = words + i+"\n"
-        with open("Page.data", 'wb') as fp:
+        with open("./pages/"+self.title, 'wb') as fp:
             pickle.dump(words, fp)
 
     def __init__(self, s):
-        self.url = s
+        self.title = s
+        self.url = "https://en.wikipedia.org/wiki/"+s
         self.data = urllib.request.urlopen(self.url)
         self.raw = self.data.read()
         self.processed=""
-        self.links=""
+        self.links=[]
         self.process()
 
