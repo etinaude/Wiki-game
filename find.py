dd7492933da63266a68316d054e98eb16e2aa1d7
etienne naude <etinaude@gmail.com>
2020-09-08 11:32:59 +1200

    added jumping to a random node if completed

--- find.py
+++ find.py
@@ -16,21 +16,25 @@ except:
     pass
 
 for i in range(6152223):
-    try:
+    #try:
         current = Page(item)
         articles.extend(current.links)
         short[current.title]= current.links
-        while articles[0] in complete:
-            articles.pop(0)
+        try:
+            while articles[0] in complete:
+                articles.pop(0)
+        except Exception as error:
+            print("error",error)
+            articles = ["Special:Random"]
         item = articles[0]
         complete.append(item)
         articles.pop(0)
         print(item)
         #print(complete)
-        if (i%20==0):
+        if (i%100==0):
             with open("json/done.json", 'w') as f:
                 json.dump(complete, f)
-    except Exception as error:
-        print(error)
-        with open("json/error.json", 'w') as f:
-                json.dump(complete, f)
+    #except Exception as error:
+    #    print("error",error)
+    #    with open("json/error.json", 'w') as f:
+    #            json.dump(complete, f)
