5701e7294027f8f6e433c683b9bf21a2177b0565
etienne naude <etinaude@gmail.com>
2020-09-08 01:18:07 +1200

    speed up

--- find.py
+++ find.py
@@ -7,10 +7,22 @@ from page import Page
 item = "Harry_Potter"
 
 articles =[]
+complete = []
+try:
+    complete = pickle.load( open( "done", "rb" ) )
+except:
+    pass
 
-for i in range(5):
+for i in range(20):
+    #print(complete)
     current = Page(item)
     articles.extend(current.links)
+    while articles[0] in complete:
+        articles.pop(0)
     item = articles[0]
+    complete.append(item)
     articles.pop(0)
     print(item)
+    if (i%5==0):
+        print("here")
+        pickle.dump( complete, open( "done", "wb" ) )
